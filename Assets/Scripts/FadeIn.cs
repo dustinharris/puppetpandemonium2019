8f6235f456abdd20f606b7428f40e4900091dd2f
Kaylee Breshon <blackcatrusting@gmail.com>
2018-02-07 22:27:22 -0800

    Added slide in and fade in scripts

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public float Duration = 1;
+    public float Delay = 0;
+    private float StartTime;
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        Color temp = image.color;
+        temp.a = 0;
+        image.color = temp;
+
+        StartTime = Time.time + Delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > StartTime)
+        {
+            float t = (Time.time - StartTime) / Duration;
+            Color temp = image.color;
+            temp.a = t;
+            image.color = temp;
+
+        }
+        
+	}
+}
---
+++ Assets/Scripts/SlideIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideIn : MonoBehaviour {
+
+
+    [SerializeField]
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+    public float Duration = 1;
+    public float Delay = 0;
+    private float StartTime;
+
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = StartPosition;
+
+
+        StartTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = (Time.time - StartTime) / Duration;
+        transform.localPosition = new Vector3(
+            Mathf.SmoothStep(StartPosition.x, EndPosition.x, t),
+            Mathf.SmoothStep(StartPosition.y, EndPosition.y, t),
+            Mathf.SmoothStep(StartPosition.z, EndPosition.z, t));
+	}
+}
