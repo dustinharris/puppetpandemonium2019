3be230b4498419a3cddc811129611aa3f87c3f5e
Kaylee Breshon <blackcatrusting@gmail.com>
2018-02-15 21:03:32 -0800

    Started writing scripts for audience participation and ammo tracking in Dont Shoot Grandma

---
+++ Assets/Scripts/GrandmaAudience.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandmaAudience : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject AudienceBar;
+    private AudienceBarScript AudienceScript;
+
+    void Start () {
+        AudienceScript = AudienceBar.GetComponent<AudienceBarScript>();
+        AudienceScript.ShowAll(AudienceUIScript.Notice.Glow);
+    }
+
+    void Update()
+    {
+        // Check if each audience member is pressing a button
+        int sections = AudienceScript.Size();
+        for (int i = 0; i < sections; i++)
+        {
+            if (Input.GetButtonDown("Audience" + i + "Red"))
+            {
+                if (NeedsReload(true))
+                {
+
+                } else
+                {
+                    
+                }
+            }
+            if (Input.GetButtonDown("Audience" + i + "Blue"))
+            {
+                if (NeedsReload(false))
+                {
+
+                } else
+                {
+
+                }
+            }
+        }
+    }
+
+    private bool NeedsReload(bool red)
+    {
+        return false;
+    }
+}
