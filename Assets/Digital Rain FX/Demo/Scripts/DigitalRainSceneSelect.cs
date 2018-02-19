b472705fc7664830fe19c54c4630c2cee6689bc4
TWTolman <Twtolman@gmail.com>
2018-02-18 16:47:46 -0800

    wheels fix

---
+++ Assets/Digital Rain FX/Demo/Scripts/DigitalRainSceneSelect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DigitalRainSceneSelect : MonoBehaviour
+{
+    public void LoadDigitalDemo01()
+    {
+        SceneManager.LoadScene("DigiRain01");
+    }
+    public void LoadDigitalDemo02()
+    {
+        SceneManager.LoadScene("DigiRain02");
+    }
+	public void LoadDigitalDemo03()
+    {
+        SceneManager.LoadScene("DigiRain03");
+    }
+	public void LoadDigitalDemo04()
+    {
+        SceneManager.LoadScene("DigiRain04");
+    }
+	public void LoadDigitalDemo05()
+    {
+        SceneManager.LoadScene("DigiRain05");
+    }
+	public void LoadDigitalDemo06()
+    {
+        SceneManager.LoadScene("DigiRain06");
+    }
+}
