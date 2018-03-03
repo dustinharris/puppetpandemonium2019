c8ce20500f507391f29b637ebc1ccb3500590d8f
Nicky Breshon <blackcatrusting@gmail.com>
2018-03-02 19:38:13 -0800

    Fixed bug where bouncing audience alerts could float away

---
+++ Assets/Scripts/SynchronizedPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynchronizedPosition : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject SynchronizationObject;
+
+	void Update () {
+        transform.localPosition = SynchronizationObject.transform.localPosition;
+	}
+}
