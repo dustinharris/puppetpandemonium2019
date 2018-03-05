4f000effeb1af6b1585000a8cee194a7a8358efa
Kaylee Breshon <blackcatrusting@gmail.com>
2018-03-04 19:20:51 -0800

    Added hearts that pop up when audience helps player escape bunnies

---
+++ Assets/Scripts/EKO2Y/AudienceHeartController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceHeartController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] RedHearts;
+    [SerializeField]
+    private GameObject[] BlueHearts;
+
+    public void ShowHeart(bool red, int index)
+    {
+        GameObject[] Hearts = red ? RedHearts : BlueHearts;
+        GameObject heart = Hearts[index];
+
+        if (heart.activeSelf)
+        {
+            heart.SetActive(false);
+        }
+
+        StartCoroutine(Show(heart));
+    }
+
+    private IEnumerator Show(GameObject heart)
+    {
+        heart.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        heart.SetActive(false);
+    }
+
+}
