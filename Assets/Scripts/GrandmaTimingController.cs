19f48ee4f0203fe03f853ae5c43dbf88d09f7d1c
Nicky Breshon <blackcatrusting@gmail.com>
2018-03-06 14:14:03 -0800

    Made camera not start moving until ready

--- Assets/Scripts/GrandmaTimingController.cs
+++ Assets/Scripts/GrandmaTimingController.cs
@@ -8,11 +8,23 @@ public class GrandmaTimingController : MonoBehaviour {
     public float MinTime = 1f;
     public float TimeDecrease = 1f;
 
+    public float TimePerPath = 2f;
+
+    [SerializeField]
+    CPC_CameraPath CameraPath;
+
     private float CurrentTime;
 
     private GrandmaUI UiScript;
 
     private Coroutine RunningCoroutine = null;
+
+    // Signal to show cutouts
+    // Wait for timeout or to receive signal back that both have been shot
+    // Signal to move locations
+    // Wait for signal that moving is finished
+    // speed up
+    // repeat
 
     private void Awake()
     {
@@ -21,16 +33,15 @@ public class GrandmaTimingController : MonoBehaviour {
         UiScript = GetComponent<GrandmaUI>();
     }
 
+    private void Start()
+    {
+        CameraPath.RefreshTransform();
+    }
+
     // Call when ready to start
     public void Ready()
     {
-
-        // Signal to show cutouts
-        // Wait for time or to receive signal back that both have been shot
-        // Signal to move locations
-        // Wait for signal that moving is finished
-        // speed up
-        // repeat
+        CameraPath.PlayPath(CameraPath.points.Count * TimePerPath);
 
         RunningCoroutine = StartCoroutine(ShowTargets());
     }
