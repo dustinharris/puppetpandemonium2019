34f1988463f4395822ad2cb0b9b0959cdb9460e0
Nicky Breshon <blackcatrusting@gmail.com>
2018-03-09 19:20:55 -0800

    Start on final final boss

---
+++ Assets/Scripts/FinalBoss/HealthBarController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+    public interface IHealthSubscriber {
+        void ZeroHealth();
+    }
+
+    [SerializeField]
+    private Image HealthBar;
+    [SerializeField]
+    private IHealthSubscriber[] Subscribers;
+    public int HitsToDead;
+
+    private float width;
+    private float perHit;
+
+	void Start () {
+        width = HealthBar.rectTransform.localScale.x;
+        perHit = width / HitsToDead;
+	}
+
+    public void DecreaseHealth()
+    {
+        width -= perHit;
+        if (width <= 0)
+        {
+            ZeroHealth();
+        }
+    }
+
+    private void ZeroHealth()
+    {
+        for (int i = 0; i < Subscribers.Length; i++)
+        {
+            Subscribers[i].ZeroHealth();
+        }
+    }
+}
---
+++ Assets/Scripts/FinalBoss/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    public interface ITimerSubscriber {
+        void Timeout();
+    }
+
+    public float TimeLimit;
+    [SerializeField]
+    private ITimerSubscriber[] subscribers;
+
+    private Text TimerText;
+
+
+	// Use this for initialization
+	void Start () {
+        TimerText = GetComponent<Text>();
+
+        StartCoroutine(Countdown());
+	}
+	
+    private IEnumerator Countdown()
+    {
+        while (TimeLimit > 0)
+        {
+            TimeLimit -= Time.deltaTime;
+            TimerText.text = ToString(TimeLimit);
+            yield return null;
+        }
+
+        Timeout();
+    }
+
+    private void Timeout()
+    {
+        for (int i = 0; i < subscribers.Length; i++)
+        {
+            subscribers[i].Timeout();
+        }
+    }
+
+    private string ToString(float time)
+    {
+        return time.ToString("00.000");
+    }
+}
