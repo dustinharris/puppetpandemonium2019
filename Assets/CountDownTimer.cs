f5296c122fb7575d2ca6e05dac53d4f8c750477a
TWTolman <Twtolman@gmail.com>
2018-03-11 00:37:03 -0800

    final boss stuff

---
+++ Assets/CountDownTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public float timeLeft = 0.0f;
+
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timeLeft  -= Time.deltaTime;
+        text.text =  "IMPACT" + timeLeft ;
+      
+    }
+}
