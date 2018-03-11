85addc904664b9fc00b502e7d6291eef46834006
TWTolman <Twtolman@gmail.com>
2018-03-10 19:40:20 -0800

    animaitions and more art

---
+++ Assets/Rotate3D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate3D : MonoBehaviour
+{
+
+    public float rotationSpeed = 1;
+    public Vector3 rotationAxis = Vector3.up;
+
+    void Update()
+    {
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime, Space.World);
+    }
+}
