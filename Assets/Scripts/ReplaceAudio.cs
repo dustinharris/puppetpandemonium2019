05d0e72369399d14b5737f6bc8e9a1037cd84bc0
Kaylee Breshon <blackcatrusting@gmail.com>
2018-03-16 15:58:13 -0700

    Added audio to first scenes

---
+++ Assets/Scripts/ReplaceAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceAudio : MonoBehaviour {
+
+    [SerializeField]
+    private string AudioObjectName = "BGAudio";
+
+	// Use this for initialization
+	void Start () {
+        GameObject oldAudio = GameObject.Find(AudioObjectName);
+        if (oldAudio != null)
+        {
+            Destroy(oldAudio);
+        }
+	}
+}
