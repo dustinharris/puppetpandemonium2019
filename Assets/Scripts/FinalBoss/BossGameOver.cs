b88b56a99a0f4849c0136198817ca791d2d14ec8
Kaylee Breshon <blackcatrusting@gmail.com>
2018-03-17 12:27:48 -0700

    Final boss tweaks

--- Assets/Scripts/FinalBoss/BossGameOver.cs
+++ Assets/Scripts/FinalBoss/BossGameOver.cs
@@ -6,10 +6,17 @@ public class BossGameOver : MonoBehaviour {
 
     [SerializeField]
     private SceneSwitcher sceneSwitcher;
+    [SerializeField]
+    private AudienceBarScript AudienceBar;
 
     private void Awake()
     {
         Messenger.AddListener(GameEvent.BOSS_GAME_OVER, GameOver);
+    }
+
+    private void Start()
+    {
+        AudienceBar.ShowAll(AudienceUIScript.Notice.Glow);
     }
 
     private void GameOver()
