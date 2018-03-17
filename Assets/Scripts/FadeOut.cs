124696751831d79d517eda5174050345d8a1ee2d
Kaylee Breshon <blackcatrusting@gmail.com>
2018-03-17 10:48:25 -0700

    Added fade out to hit text

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+
+    public float Duration = 1;
+    public float Delay = 0;
+    private float StartTime;
+    private Image image;
+    private Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        image = GetComponent<Image>();
+        if (image == null)
+        {
+            text = GetComponent<Text>();
+        }
+        Color temp = GetColor();
+        temp.a = 1;
+        SetColor(temp);
+
+        StartTime = Time.time + Delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > StartTime)
+        {
+            float t = (Time.time - StartTime) / Duration;
+            Color temp = GetColor();
+            temp.a = 1 - t;
+            SetColor(temp);
+        }
+    }
+
+    private Color GetColor()
+    {
+        if (image != null)
+        {
+            return image.color;
+        }
+        else
+        {
+            return text.color;
+        }
+    }
+
+    private void SetColor(Color c)
+    {
+        if (image != null)
+        {
+            image.color = c;
+        } else
+        {
+            text.color = c;
+        }
+    }
+}
