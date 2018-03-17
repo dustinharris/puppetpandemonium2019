72b2eada240af447c067f1d32e2e7fc2339a9fc6
Kaylee Breshon <blackcatrusting@gmail.com>
2018-03-16 18:24:51 -0700

    Pause audio for vortex then unpause in next scene

--- Assets/Scripts/ReplaceAudio.cs
+++ Assets/Scripts/ReplaceAudio.cs
@@ -6,13 +6,36 @@ public class ReplaceAudio : MonoBehaviour {
 
     [SerializeField]
     private string AudioObjectName = "BGAudio";
+    [SerializeField]
+    private bool Pause = false;
+
+    private AudioSource audio;
 
 	// Use this for initialization
 	void Start () {
         GameObject oldAudio = GameObject.Find(AudioObjectName);
         if (oldAudio != null)
         {
-            Destroy(oldAudio);
+            if (Pause)
+            {
+                audio = oldAudio.GetComponent<AudioSource>();
+                if (audio != null)
+                {
+                    audio.Pause();
+                }
+            }
+            else
+            {
+                Destroy(oldAudio);
+            }
         }
 	}
+
+    private void OnDestroy()
+    {
+        if (audio != null)
+        {
+            audio.UnPause();
+        }
+    }
 }
